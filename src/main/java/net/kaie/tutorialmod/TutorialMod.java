8f5010cbf185f06b85f01a0fa0d19c7eefc71de9
Nightshade-Faraday <graywolf434@gmail.com>
2023-05-11 22:27:12 -0400

    Initial commit

---
+++ src/main/java/net/kaie/tutorialmod/TutorialMod.java
@@ -0,0 +1,15 @@
+package net.kaie.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String TUTORIALMOD = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(TUTORIALMOD);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
---
+++ src/main/java/net/kaie/tutorialmod/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.kaie.tutorialmod.mixin;
+
+import net.kaie.tutorialmod.TutorialMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		TutorialMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
