0fc5dd45255c89cdad01cc876c444343cdeccc69
Nightshade-Faraday <graywolf434@gmail.com>
2023-05-11 22:28:35 -0400

    added important comment

--- src/main/java/net/kaie/tutorialmod/mixin/ExampleMixin.java
+++ src/main/java/net/kaie/tutorialmod/mixin/ExampleMixin.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+//Very important comment
 @Mixin(TitleScreen.class)
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
